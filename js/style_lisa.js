3efc6933531b23feccecab19a20a87f7d070b32b
Lorina Schäfer <suryalorina@gmail.com>
2021-09-05 10:55:08 +0200

    Lisas Datein hinzugefügt

---
+++ js/style_lisa.js
@@ -0,0 +1,7 @@
+document.getElementById('buttonBuchen').addEventListener("click", function() {
+	document.querySelector('.hintergrund').style.display = "flex";
+});
+
+document.querySelector('.zu').addEventListener("click", function() {
+	document.querySelector('.hintergrund').style.display = "none";
+});
